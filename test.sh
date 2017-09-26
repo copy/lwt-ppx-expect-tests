8f744197e3be53d04c660415346c69d1bafd3137
Fabian <copy@copy.sh>
2017-09-26 14:37:51 +0200

    lwt ppx expect tests for let, match and try

--- test.sh
+++ test.sh
@@ -1,1 +1,12 @@
 set -e
+ocaml -noinit let_1.ml 2>&1 | diff - let_1.expect
+ocaml -noinit let_2.ml 2>&1 | diff - let_2.expect
+ocaml -noinit let_3.ml 2>&1 | diff - let_3.expect
+ocaml -noinit let_4.ml 2>&1 | diff - let_4.expect
+ocaml -noinit match_1.ml 2>&1 | diff - match_1.expect
+ocaml -noinit match_2.ml 2>&1 | diff - match_2.expect
+ocaml -noinit match_3.ml 2>&1 | diff - match_3.expect
+ocaml -noinit match_4.ml 2>&1 | diff - match_4.expect
+ocaml -noinit try_1.ml 2>&1 | diff - try_1.expect
+ocaml -noinit try_2.ml 2>&1 | diff - try_2.expect
+ocaml -noinit try_3.ml 2>&1 | diff - try_3.expect
